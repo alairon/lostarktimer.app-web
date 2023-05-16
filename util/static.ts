b7fd97bab0773e8459996a13b6116053ac036478
Ambrose Lai <alairon@outlook.com>
2023-05-16 13:14:20 -0400

    fix: SA Time adjustment

--- util/static.ts
+++ util/static.ts
@@ -5,5 +5,5 @@ export const RegionTimeZoneMapping: { [K in RegionKey]: string } = {
   'US East': 'UTC-4',
   'EU Central': 'UTC+1',
   'EU West': 'UTC-0',
-  'South America': 'UTC-4',
+  'South America': 'UTC-3',
 }
